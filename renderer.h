9782e043a68c16a5857ec023c953c95ad8a6db13
R3SH <alekss-reshetov@mail.ru>
2020-09-11 18:47:37 -0700

    Renderer rework

--- renderer.h
+++ renderer.h
@@ -1,5 +1,11 @@
+#pragma once
+
 #include <iostream>
 #include <GL/glew.h>
+
+#include "VertexArray.h"
+#include "IndexBuffer.h"
+#include "Shader.h"
 
 #define ASSERT(x) if (!(x)) __debugbreak();
 
@@ -10,3 +16,10 @@
 void GLClearError();
 
 bool GLLogCall(const char* function, const char* file, int line);
+
+class Renderer
+{
+public:
+	void Clear() const;
+	void Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader) const;
+};
