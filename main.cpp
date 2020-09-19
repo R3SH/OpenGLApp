9e1c8eaaabb342c2d4c2ff02e1225e997ff11560
R3SH <alekss-reshetov@mail.ru>
2020-09-19 10:18:20 -0700

    ImGui fix

--- main.cpp
+++ main.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:96016076b0b9a068d0aa892d515a3defd2b8d22402b4d2bfce28be8fc4b6343d
-size 2656
+oid sha256:e71c194f0d3ba2fcd791b4fff0b3473b79b840976ef18521d90ecfe37f29d76d
+size 3361
